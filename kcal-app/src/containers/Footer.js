025137c782e24eb6d27e07b9d6711cbc8775c8a4
Jayden-Briffa <jaydenbriffa@outlook.com>
2025-03-20 00:25:23 +0000

    Style footer

--- kcal-app/src/containers/Footer.js
+++ kcal-app/src/containers/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../styles/Footer.css';
 
 function Footer() {
   return (
-    <footer className="footer bg-pink mt-0">
-      <p>Created by: Jayden Briffa</p>
+    <footer className="footer bg-pink d-flex flex-row align-content-center m-0">
+      <p className="flex-grow-1 align-self-center m-0">Created by: Jayden Briffa</p>
     </footer>
   );
 }
