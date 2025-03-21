3cfd4d2b164b978c02f04134f4179bb1713ca54c
Jayden-Briffa <jaydenbriffa@outlook.com>
2025-03-21 23:20:23 +0000

    Fix getfoods query before endpoint

--- kcal-app/src/services/getFoods.js
+++ kcal-app/src/services/getFoods.js
@@ -2,14 +2,15 @@
 export default async function getFoods(foodId = null){
 
     try{
-        let url = `http://localhost:4001/api/foods?orderedBy=name`;
-    
+        let url = `http://localhost:4001/api/foods`;
+        let query = '?orderedBy=name';
+
         if (foodId){
             url += `/${foodId}`;
         }
 
         // Get a response from the API and translate to JSON
-        const response = await fetch(url);
+        const response = await fetch(url + query);
         const jsonResponse = await response.json();
 
         console.log("FOODS: ", jsonResponse)
