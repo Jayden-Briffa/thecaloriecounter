e1c7c1a38718e842cc8291ba82a346dca07dd4c1
Jayden-Briffa <jaydenbriffa@outlook.com>
2025-04-04 17:48:34 +0100

    Allow the GET /foods endpoint to be called with ?foodIds=1,2,3

--- kcal-app/src/containers/DashboardPage.js
+++ kcal-app/src/containers/DashboardPage.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import DashboardOutput from './DashboardOutput';
 import { ProcessesProvider } from '../context/LoadingProcessesContext';
+import getFoods from '../services/getFoods';
 
 function DashboardPage() {
+  useEffect(() => {
+    async function foodsRes(){
+      const res = await getFoods({foodId: [1,2,3]})
+      console.log(res)
+    }
+
+    foodsRes();
+  }, [])
 
   return (
     <ProcessesProvider>
 
       <section className="page">
         <h1 className="mb-5">Dashboard</h1>
-        <DashboardOutput />
+        {/* <DashboardOutput /> */}
 
       </section>
       
