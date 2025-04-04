e21ef1d1611684d8feee33d78ecb395b5072b892
Jayden-Briffa <jaydenbriffa@outlook.com>
2025-04-04 18:10:52 +0100

    Make today's foods page use 1 GET request to get all necessary foodData

--- kcal-app/src/containers/TodaysFoodsOutput.js
+++ kcal-app/src/containers/TodaysFoodsOutput.js
@@ -41,27 +41,13 @@ function TodaysFoodsTableOutput() {
 
   useEffect(() => {
     async function fetchData(){
-      let tableData = [];
-  
       // Use a for loop to access await
       // food holds consumed id, food id, consumed quantity, consumed kcal, and date
       // newFoodData holds id, name, quantity, units, kcal, and date
-      for (const food of consumedFoods){
-        const newFoodData = await getFoods({foodId: food.food_id});
-        let newTableData = {};
-    
-        newTableData['id'] = food.id
-        newTableData['food_id'] = food.food_id
-        newTableData['name'] = newFoodData.name
-        newTableData['quantity'] = food.quantity;
-        newTableData['units'] = newFoodData.units;
-        newTableData['kcal'] = food.kcal;
-        newTableData['dateConsumed'] = newFoodData.date_consumed;
+      let newFoodsData = await getFoods({foodId: [consumedFoods.map(food => food.food_id)]});
+      newFoodsData = newFoodsData['Foods'];
 
-        tableData.push(newTableData)
-      }
-    
-      setFoodData(tableData)
+      setFoodData(newFoodsData);
     }
     
     if (consumedFoods){
