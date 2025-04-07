e87ec19ccede942abd87f840369e34b6b1244960
Jayden-Briffa <jaydenbriffa@outlook.com>
2025-04-07 12:40:28 +0100

    Improve styling when inputs and selects have :focus

--- kcal-app/src/components/DashboardDaysForm.js
+++ kcal-app/src/components/DashboardDaysForm.js
@@ -4,7 +4,7 @@ function component(props) {
   return (
     <div className='mt-4'>
       <form className='d-flex justify-content-center gap-4' onSubmit={props.handleSubmit}>
-          <p className='align-self-center text-center mb-0'>Show the last <input type="number" min="0" max="999" className='border-pink border-1 rounded rounded-2 text-center' id="daysInput" value={props.selectedDays} onChange={props.changeHandler} /> days</p>
+          <p className='align-self-center text-center mb-0'>Show the last <input type="number" min="0" max="999" className='form-control d-inline border-pink border-1 rounded rounded-2 text-center py-0' id="daysInput" value={props.selectedDays} onChange={props.changeHandler} /> days</p>
           <button type="submit" className="button align-self-center">Show</button>
       </form>
     </div>
